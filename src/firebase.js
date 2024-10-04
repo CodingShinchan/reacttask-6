1a86ebd5b4edc2a5f0ee0d7885d2eeae61aeb3ee
Poornima Kumawat <89656087+CodingShinchan@users.noreply.github.com>
2024-10-04 08:32:09 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA_geq5WWS2E8oj6piKUWbWtMYhClbO9nk",
-  authDomain: "task-6-5b4a3.firebaseapp.com",
-  projectId: "task-6-5b4a3",
-  storageBucket: "task-6-5b4a3.appspot.com",
-  messagingSenderId: "406316034459",
-  appId: "1:406316034459:web:0cbc1b64bcd936f2b3593e",
-  measurementId: "G-CD5NMP10HF",
+  apiKey: "AIzaSyC6_-XkSmD_GHw8OgymS8lmGCpeVvBuu3Q",
+  authDomain: "react6-1c7d0.firebaseapp.com",
+  databaseURL: "https://react6-1c7d0-default-rtdb.firebaseio.com",
+  projectId: "react6-1c7d0",
+  storageBucket: "react6-1c7d0.appspot.com",
+  messagingSenderId: "962791756043",
+  appId: "1:962791756043:web:ec5d5ff359b9fa1732120e",
+  measurementId: "G-WHV1S289Y5"
 };
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
